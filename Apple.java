1f7bab055d6798b50d59d47b5b67a47fd2e0680e
Florian Garms <93873756+Birnendampf@users.noreply.github.com>
2024-04-17 08:11:01 +0200

    added basic fruit implementation and apple

---
+++ Apple.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Apple here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Apple extends Fruit
+{
+    public int getValue() {
+        return 1;
+    }
+    public int getScore() {
+        return 1;
+    }
+}
---
+++ Fruit.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fruit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public abstract class Fruit extends Actor
+{
+    public abstract int getValue();
+    public abstract int getScore();
+}
