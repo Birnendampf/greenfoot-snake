b684436f34f176451ad916df20944feb5267e8be
Florian Garms <93873756+Birnendampf@users.noreply.github.com>
2024-04-24 22:43:19 +0200

    final final commit

--- MyButton.java
+++ MyButton.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyButton extends Button
 {
     protected void onClicked() {
-        Greenfoot.stop();
+        System.out.println("clicked!");
     }
 
     protected void changePressedState(boolean pressed) {
